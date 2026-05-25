fb75ebfec2babb08ec113eda77849176f7b92e78
AIProGuru <ai.pro961212@gmail.com>
2026-05-24 22:18:34 -0700

    fix firbase config

--- frontend/src/FirebaseConfig.js
+++ frontend/src/FirebaseConfig.js
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgc1FYHNse7kW4QC4jlSt8jTLdoVd5hxw",
-    authDomain: "heliorcm-46d2b.firebaseapp.com",
-    projectId: "heliorcm-46d2b",
-    storageBucket: "heliorcm-46d2b.firebasestorage.app",
-    messagingSenderId: "394628458918",
-    appId: "1:394628458918:web:9354908d5f20268c51c767",
-    measurementId: "G-3BF5S1CZC4"
-};
+    apiKey: "AIzaSyBNAzDL2b5g0TaXTZVocilayMP59sbufR4",
+    authDomain: "heliorcm-e8d7a.firebaseapp.com",
+    projectId: "heliorcm-e8d7a",
+    storageBucket: "heliorcm-e8d7a.firebasestorage.app",
+    messagingSenderId: "763930456497",
+    appId: "1:763930456497:web:18d1ad65ff112aa35b8d90",
+    measurementId: "G-Z4G00NTT0M"
+  };
 
 // const firebaseConfig = {
 //     apiKey: "AIzaSyDijNOhO7agUS6aDOPkb_Dkzfmn1z3XNxE",
